9d81a5757aa0dfb7ca68edccd081bdf591c6df9e
Maxim Avanov <maxim.avanov@gmail.com>
2021-07-16 23:42:50 +0100

    ghc env

--- default.nix
+++ default.nix
@@ -1,17 +1,26 @@
 {}:
 
 let
-    pkgs = import (builtins.fetchTarball {
+    overlay = (self: original: rec {
+        # Place overrides here as described in https://nixos.wiki/wiki/Overlays#Examples_of_overlays
+
+        # Upstream ghcid doesn't expose ghc(i) as a propagated dependency, so we fix it on our side
+        ghcid = original.ghcid.overrideAttrs (oldAttrs: {
+            propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [ original.ghc ];
+        });
+    });
+
+    nixpkgs-src = builtins.fetchTarball {
          # Descriptive name to make the store path easier to identify
          name   = "nix-common-snapshot";
          # Commit hash for nixos-unstable as of 2021-04-29
          url    = https://github.com/NixOS/nixpkgs/archive/79225c623d2f07f095251d3e509cc6a4cee6119e.tar.gz;
          # Hash obtained using `nix-prefetch-url --unpack <url>`
          sha256 = "02j88v5yyvh1ximdi4wn46mb5vfrl0rdmbmv2mi644gh4wzfn4x3";
-    }) {};
+    };
 
 in
 
 {
-    inherit pkgs;
+    pkgs = import nixpkgs-src { overlays = [ overlay ]; };
 }
---
+++ ghc-env.nix
@@ -0,0 +1,35 @@
+{
+    pkgs                    ? (import ./. {}).pkgs
+,   haskellCompiler         ? "ghc8104"
+,   haskellHackageOverrides ? (self: original: {})  # Project-specific Hackage overrides
+,   haskellLibraries        ? (hackagePackageSet: with hackagePackageSet; [ ])
+,   localDevTools           ? (ps: with ps; [ gnumake
+                                                  gitAndTools.pre-commit
+                                                  lsof
+                                                  which
+                                                  libiconv  # required for building Cabal
+                                                  cabal2nix ])
+
+
+}:
+
+let
+    ghcVariant             = pkgs.haskell.packages;
+    ghcPkgSetWithOverrides = ghcVariant.${haskellCompiler}.override {
+        overrides = self: original: {
+            # not yet
+        } // haskellHackageOverrides self original;  # apply (union) project-specific Hackage overrides after global overrides
+    };
+
+    # `ghc` is a derivation that contains GHC + required project libraries,
+    # we can use it other shells and derivations as a single derivation
+    # that represents our Haskell project with all its dependencies
+    ghc = ghcPkgSetWithOverrides.ghcWithPackages haskellLibraries;
+
+in
+
+{
+    inherit pkgs;
+    inherit ghc;
+    localTooling = [ ghc ] ++ (localDevTools pkgs);
+}
