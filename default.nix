6e47c05632c781e0279f4b4609fb4125e4e7bf67
Max <maxim.avanov@gmail.com>
2022-09-15 23:27:22 +0300

    common overlays

--- default.nix
+++ default.nix
@@ -2,15 +2,6 @@
 }:
 
 let
-    overlay = (self: original: rec {
-        # Place overrides here as described in https://nixos.wiki/wiki/Overlays#Examples_of_overlays
-
-        # Upstream ghcid doesn't expose ghc(i) as a propagated dependency, so we fix it on our side
-        ghcid = original.ghcid.overrideAttrs (oldAttrs: {
-            propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [ original.ghc ];
-        });
-    });
-
     nixpkgs-src = builtins.fetchTarball {
          # Descriptive name to make the store path easier to identify
          name   = "nix-common-snapshot";
@@ -20,8 +11,10 @@ let
          sha256 = "0n9cajpbhsga77ssq8axvy7b28x4p7phkx4nb1216b07ar87qzim";
     };
 
+    commonOverlays = (import ./overlays.nix {}).overlays;
+
 in
 
 {
-    pkgs = import nixpkgs-src { overlays = [ overlay ] ++ projectOverlays; };
+    pkgs = import nixpkgs-src { overlays = [ commonOverlays ] ++ projectOverlays; };
 }
---
+++ overlays.nix
@@ -0,0 +1,40 @@
+#
+# Place global overrides and extra derivations here as described in https://nixos.wiki/wiki/Overlays#Examples_of_overlays
+#
+{}:
+
+let
+
+postgres-local-management-src = builtins.fetchTarball {
+    name   = "postgres-local-management-src";
+    url    = https://github.com/avanov/postgres-local-management/archive/f8d96a5751176990512a602d1f9bffdc9ee0334a.tar.gz;
+    sha256 = "sha256:105kzq3dprzdmbi0d3nr3p3wx45vv4k3smf2y17bh01nwy65a3ld";
+};
+
+overlays = (self: original: rec {
+    # Upstream ghcid doesn't expose ghc(i) as a propagated dependency, so we fix it on our side
+    ghcid = original.ghcid.overrideAttrs (oldAttrs: {
+        propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [ original.ghc ];
+    });
+
+    postgres-local-management = self.stdenv.mkDerivation {
+        name    = "postgres-local-management";
+        src     = postgres-local-management-src;
+        wget    = self.wget;
+        builder = "${self.bash}/bin/bash";
+        args    = [
+            (self.writeScript "wait-for-builder"
+                ''
+                    source $stdenv/setup
+                    install -d $out/
+                    cp $src/Makefile $out/Makefile
+                ''
+            )];
+        };
+});
+
+in
+
+{
+    inherit overlays;
+}
