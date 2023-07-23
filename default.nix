a5ec318f10373629c9c626dbcaecd2f7f534e249
Maxim Avanov <601955+avanov@users.noreply.github.com>
2023-07-23 22:57:38 +0100

    upd

--- default.nix
+++ default.nix
@@ -5,9 +5,9 @@ let
     nixpkgs-src = builtins.fetchTarball {
          # Descriptive name to make the store path easier to identify
          name   = "nix-common-snapshot-2023-06-21";
-         url    = https://github.com/NixOS/nixpkgs/archive/3556804b333d0a810e0c99a853bf49dc02c46c5a.tar.gz;
+         url    = https://github.com/NixOS/nixpkgs/archive/10db592f7d6af1985a5dccf337cfd7cc5ab3924e.tar.gz;
          # Hash obtained using `nix-prefetch-url --unpack <url>`
-         sha256 = "sha256:1j16ybclfswjavd5pns4wgq7ijlcfrxkl2fbkvcd0za4s9pla9wg";
+         sha256 = "sha256:193qbrcwqdmagny55vg97zswrklqnw10arq0fylp68s7dgiz6qqc";
     };
 
     commonOverlays = (import ./overlays.nix {}).overlays;
